48c27e5996d9c1f51a96a6dab57acbeabfd3c9dc
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-09-16 09:52:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection error: ", err);
+    await mongoose.connect(process.env.MONGO_URI); 
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
